dab918cd1ff61a000b758fa71664fabe831b881b
Sara Giannini <97319856+Sara-Giannini@users.noreply.github.com>
2022-04-22 18:55:26 -0300

    Update exemplo1.js

--- exemplo1.js
+++ exemplo1.js
@@ -5,12 +5,16 @@
 // b) quando chegar no número 25 interrompa a instrução e pare o loop
 // c) quando chegar no número 10 pule a instrução|
 
-
+--- | --- |---|
 //For
 
  for(let conta = 1; conta <= 50; conta = conta + 1) 
  console.log(conta)
+    
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 
+26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
 
+--- | --- |---|
 //Break
 
   for(let quebra = 1; quebra <= 50;  quebra = quebra + 1) {
@@ -21,6 +25,9 @@
       }  
     }
 
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+
+--- | --- |---|
 // Continue
 
  let continua = 0
@@ -31,6 +38,9 @@
      console.log("continuar")
      continue
    }
-
    console.log(continua)
 }
+
+1 2 3 4 5 6 7 8 9 continuar 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 
+26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+
