e12a98485f3bfbd8aca58ad4345507f3d6a92867
Sara Giannini <97319856+Sara-Giannini@users.noreply.github.com>
2022-04-22 18:58:16 -0300

    Update exemplo4.js

--- exemplo4.js
+++ exemplo4.js
@@ -8,3 +8,55 @@ for(let i = 0; i <= 100; i++){
         console.log(i,'é par')
     }
 }
+
+0 é par
+2 é par
+4 é par
+6 é par
+8 é par
+10 é par
+12 é par
+14 é par
+16 é par
+18 é par
+20 é par
+22 é par
+24 é par
+26 é par
+28 é par
+30 é par
+32 é par
+34 é par
+36 é par
+38 é par
+40 é par
+42 é par
+44 é par
+46 é par
+48 é par
+50 é par
+52 é par
+54 é par
+56 é par
+58 é par
+60 é par
+62 é par
+64 é par
+66 é par
+68 é par
+70 é par
+72 é par
+74 é par
+76 é par
+78 é par
+80 é par
+82 é par
+84 é par
+86 é par
+88 é par
+90 é par
+92 é par
+94 é par
+96 é par
+98 é par
+100 é par
